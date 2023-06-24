441d898ab779997435fb9e75ffb927ef3c307176
Kazuki Takahashi <tkhsh.schoen@gmail.com>
2023-06-24 13:45:32 +0900

    Add 04 and 05

---
+++ 04_static_library/lib.cpp
@@ -0,0 +1,15 @@
+// Copyright 2023 AMSL
+
+#include <iostream>
+
+#include "lib.h"
+
+namespace lib
+{
+  // 実装
+  void print_hello_world()
+  {
+    std::cout << "Hello, world! from " << __FILE__ << std::endl;
+
+  }
+}
---
+++ 05_dynamic_library/main.cpp
@@ -0,0 +1,11 @@
+// Copyright 2023 AMSL
+
+#include <iostream>
+
+#include "lib.h"
+
+int main(int argc, char **argv)
+{
+  lib::print_hello_world();
+  return 0;
+}
